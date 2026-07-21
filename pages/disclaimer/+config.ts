2542f54fa6e924068cc21ce1fc7b587929b2e25d
wzpsekx194481 <wzpsekx194481@hotmail.com>
2026-07-21 10:08:41 +0800

    Update +config.ts

--- pages/disclaimer/+config.ts
+++ pages/disclaimer/+config.ts
@@ -1,7 +1,7 @@
 export default {
-  title: "免责声明",
-  description: "本站服务免责相关条款",
-  prerender: true, // 关键，缺少会无法访问
+  title: "隐私政策 - EK发卡商城",
+  description: "本站隐私政策，说明用户数据收集、支付、邮件信息处理规则",
+  prerender: true, // 构建时预渲染为静态HTML，利于SEO
   meta: [
     { name: "robots", content: "index,follow" }
   ]
