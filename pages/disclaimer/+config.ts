fc15a3768ae627c0e4937788a3ae5c38ff0bf8fd
wzpsekx194481 <wzpsekx194481@hotmail.com>
2026-07-21 10:12:58 +0800

    Update +config.ts

--- pages/disclaimer/+config.ts
+++ pages/disclaimer/+config.ts
@@ -1,6 +1,6 @@
 export default {
-  title: "隐私政策 - EK发卡商城",
-  description: "本站隐私政策，说明用户数据收集、支付、邮件信息处理规则",
+  title: "免责声明 - EK发卡商城",
+  description: "本站免责声明，说明平台服务、交易相关风险条款",
   prerender: true, // 构建时预渲染为静态HTML，利于SEO
   meta: [
     { name: "robots", content: "index,follow" }
